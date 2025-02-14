bdceb61cb4b092da0589632fe986b3a535843d62
nmset0 <nmset0@colostate.edu>
2025-02-14 14:28:31 -0700

    14 Feb

--- workspace/Processing/Output_Risk.R
+++ workspace/Processing/Output_Risk.R
@@ -5,43 +5,29 @@ library(data.table)
 rm(list=ls())
 
 # Importing data (290 datasets)
-disaster <- read_csv("~/internship/workspace/wildfire_disaster.csv")
+disaster <- read_csv("~/internship/workspace/HICAHS_States_National_Risk_Index_Counties.csv")
 
-setwd("C:/Users/natha/OneDrive/Documents/internship/workspace/Ag Output Data/Colorado")
-  files = list.files(pattern="*.csv")
-    dataset = do.call(rbind, lapply(files, fread))
-      rm(files)
-        coloradoOutput <- as.data.frame(unclass(dataset))
-setwd("C:/Users/natha/OneDrive/Documents/internship/workspace/Ag Output Data/Wyoming")
-  files = list.files(pattern="*.csv")
-    dataset = do.call(rbind, lapply(files, fread))
-      rm(files)
-        wyomingOutput <- as.data.frame(unclass(dataset))
-setwd("C:/Users/natha/OneDrive/Documents/internship/workspace/Ag Output Data/North_Dakota")
-  files = list.files(pattern="*.csv")
-    dataset = do.call(rbind, lapply(files, fread))
-      rm(files)
-        northDakotaOutput <- as.data.frame(unclass(dataset))
-setwd("C:/Users/natha/OneDrive/Documents/internship/workspace/Ag Output Data/South_Dakota")
-  files = list.files(pattern="*.csv")
-    dataset = do.call(rbind, lapply(files, fread))
-      rm(files)
-        southDakotaOutput <- as.data.frame(unclass(dataset))
-setwd("C:/Users/natha/OneDrive/Documents/internship/workspace/Ag Output Data/Utah")
-  files = list.files(pattern="*.csv")
-    dataset = do.call(rbind, lapply(files, fread))
-      rm(files)
-        utahOutput <- as.data.frame(unclass(dataset))
-setwd("C:/Users/natha/OneDrive/Documents/internship/workspace/Ag Output Data/Montana")
-    files = list.files(pattern="*.csv")
-      dataset = do.call(rbind, lapply(files, fread))
-        rm(files)
-          montanaOutput <- as.data.frame(unclass(dataset))
+states <- c("Colorado", "Wyoming", "North_Dakota", "South_Dakota", "Utah", "Montana")
+base_path <- "C:/Users/natha/OneDrive/Documents/internship/workspace/Ag Output Data"
+
+state_outputs <- list()
+
+for(state in states) {
+  setwd(file.path(base_path, state))
+
+  files <- list.files(pattern = "*.csv")
+  dataset <- do.call(rbind, lapply(files, fread))
+
+  state_outputs[[state]] <- as.data.frame(unclass(dataset))
+
+  rm(files, dataset)
+}
+
+ag_output <- do.call(rbind, state_outputs)
+
+rm(state_outputs, state, states, base_path)
 
 setwd("C:/Users/natha/OneDrive/Documents/internship")
-rm(dataset)
-
-ag_output <- rbind(coloradoOutput, northDakotaOutput, southDakotaOutput, montanaOutput, utahOutput, wyomingOutput)
 
 for (i in 1:nrow(ag_output)) {
   if (!is.na(ag_output$domain.category[i]) && ag_output$domain.category[i] != "") {
@@ -50,10 +36,10 @@ for (i in 1:nrow(ag_output)) {
 }
 
 ag_output <- ag_output %>% select(-domain.category, -state.fips, -commodity, -county.code)
+
 ag_output <- as.data.frame(lapply(ag_output, function(x) {
   if (is.character(x)) tolower(x) else x
 }))
-
 
 ag_output_wide <- ag_output %>%
   pivot_wider(names_from = data.item, values_from = value)
@@ -65,4 +51,30 @@ colnames(ag_output_wide) <- gsub(":", "", colnames(ag_output_wide))
         colnames(ag_output_wide) <- gsub(" ", "_", colnames(ag_output_wide))
           colnames(ag_output_wide) <- gsub("__", "_", colnames(ag_output_wide))
 
-view(ag_output_wide)
+# view(ag_output_wide)
+
+ag_output_wide <- ag_output_wide |>
+  add_row(county = "chaffee", state = "colorado", .before = 9) |>
+    add_row(county = "san juan", state = "colorado", .before = 57) |>
+      slice(c(1:20, 22, 21, 23:n())) |>
+        slice(c(1:33, 35, 34, 36:n()))
+#-------------------------------------------------------------------------------------------------------------#
+
+disaster <- as.data.frame(lapply(disaster, function(x) {
+  if (is.character(x)) tolower(x) else x
+}))
+
+disaster <- disaster |> select(-National.Risk.Index.ID)
+disaster$county[disaster$county == "lamoure"] <- "la moure"
+
+# view(disaster)
+#-------------------------------------------------------------------------------------------------------------#
+# combining datasets for correlations
+
+disaster <- disaster[match(paste(ag_output_wide$state, ag_output_wide$county),
+                           paste(disaster$state, disaster$county)), ]
+
+shared_cols <- intersect(names(disaster), names(ag_output_wide))
+  ag_output_unique <- ag_output_wide %>% select(-all_of(shared_cols))
+    output_risk_combined <- cbind(disaster, ag_output_unique)
+
