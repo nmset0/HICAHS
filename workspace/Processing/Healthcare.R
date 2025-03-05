0544c6d7d02541b1657a2e227e8c751b07c02e5c
nmset0 <nmset0@colostate.edu>
2025-03-04 23:10:09 -0700

    4 Mar

--- workspace/Processing/Healthcare.R
+++ workspace/Processing/Healthcare.R
@@ -1,9 +1,21 @@
-library(tidyverse)
-library(readr)
 rm(list = ls())
 
-health <- read_csv("C:/Users/natha/OneDrive/Documents/internship/workspace/PLACES__Local_Data_for_Better_Health__County_Data_2024_release_20250303.csv")
-colnames(health) <- tolower(names(health))
-state_names <- c("Colorado", "Montana", "North Dakota", "South Dakota", "Utah", "Wyoming")
-health <- filter(health, state %in% state_names) |> arrange(stateabbr)
+library(tidyverse)
+library(readr)
 
+# health <- read_csv("C:/Users/natha/OneDrive/Documents/internship/workspace/Health Facility Data/PLACES__Local_Data_for_Better_Health__County_Data_2024_release_20250303.csv")
+# colnames(health) <- tolower(names(health))
+# state_names <- c("Colorado", "Montana", "North Dakota", "South Dakota", "Utah", "Wyoming")
+# health <- filter(health, state %in% state_names) |> arrange(stateabbr)
+
+
+# colorado <- read_excel("workspace/Health Facility Data/ColoradoHealth24_xlsx.xlsx", sheet = 2)
+# wyoming <- read_excel("workspace/Health Facility Data/WyomingHealth24_xlsx.xlsx", sheet = 2)
+# south_dakota <- read_excel("workspace/Health Facility Data/SouthDakotaHealth24_xlsx.xlsx", sheet = 2)
+# north_dakota <- read_excel("workspace/Health Facility Data/NorthDakotaHealth24_xlsx.xlsx")
+# utah <- read_excel("workspace/Health Facility Data/UtahHealth24_xlsx.xlsx", sheet = 2)
+# montana <- read_excel("workspace/Health Facility Data/MontanaHealth24_xlsx.xlsx")
+
+
+
+# TODO: data merging
