b0d6918eb80116e7e2d1a436dff684b16ea7fc7f
nmset0 <nmset0@colostate.edu>
2025-04-06 16:10:17 -0600

    Small change

--- workspace/Analysis/create_correlogram_function.R
+++ workspace/Analysis/create_correlogram_function.R
@@ -14,6 +14,23 @@ generate_corr_plot <- function(df_names) {
   }
 }
 
+
+
+generate_corr_plot2 <- function(df_names) {
+  for (df_name in df_names) {
+
+    df <- get(df_name, envir = .GlobalEnv)
+    corr <- round(cor(df, method = "spearman", use = "pairwise.complete.obs"), 2)
+    matrix <- cor_pmat(df)
+    corrplot <- ggcorrplot(corr, p.mat = matrix, method = "square", type = "lower",
+                           lab = TRUE, lab_size = 1.2, insig = "blank", title = paste("Correlation Plot:", str_to_title(gsub("\\.", " ", df_name)))) +
+      theme(axis.text.x = element_text(size = 4, angle = 90, hjust = 1, vjust = -0.5),
+            axis.text.y = element_text(size = 4))
+
+    assign(paste(df_name, "_corrplot", sep = ""), corrplot, envir = .GlobalEnv)
+  }
+}
+
 # N <- c("drought", "wildfire", "Cold.Wave", "tornado", "ice.storm", "winter.weather", "strong.wind", "hail", "heat.wave", "avalanche", "Landslide", "lightning", "Riverine.Flooding")
 # # Example usage with a list of data frame names
 # generate_corr_plot(N)
