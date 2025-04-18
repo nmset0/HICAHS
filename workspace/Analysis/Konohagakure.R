e17d947fabdc64549985667b16e314301626fae3
nmset0 <nmset0@colostate.edu>
2025-04-18 16:58:13 -0600

    code

--- workspace/Analysis/Konohagakure.R
+++ workspace/Analysis/Konohagakure.R
@@ -108,6 +108,11 @@ JOIN <- JOIN |> select(-`Critical Access Hospitals`, -Critical_Access_Hospitals)
 JOIN$county[JOIN$county == "lamoure"] <- "la moure"
 
 JOIN$H2A_workers[is.na(JOIN$H2A_workers)] <- 0
+
+
+JOIN <- JOIN |>
+  group_by(state) |>
+  mutate(H2AStateTotal = sum(H2A_workers, na.rm = TRUE))
 #---------------------------------------------------------------------------------------------------------------#
 JOIN_CUT <- JOIN |>
   select(
